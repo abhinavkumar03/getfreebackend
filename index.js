df08c088f1471a4dd8fc803f093a8d0be92dc5f2
abhinavkumar03 <ak2711474@gmail.com>
2023-06-13 12:49:54 +0530

    first commit

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import cors from "cors";
 import mongoose from "mongoose";
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 
 const app = express();
-dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
@@ -17,10 +19,13 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = "mongodb://ak2711474:ak2711474@ac-pjsrkgs-shard-00-00.dxx9jxd.mongodb.net:27017,ac-pjsrkgs-shard-00-01.dxx9jxd.mongodb.net:27017,ac-pjsrkgs-shard-00-02.dxx9jxd.mongodb.net:27017/?ssl=true&replicaSet=atlas-6w9zwt-shard-0&authSource=admin&retryWrites=true&w=majority"
-const PORT = process.env.CONNECTION_URL || 5000;
+app.get('/', (req,res) => {
+    res.send('APP IS RUNNING');
+});
 
-mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((error) => console.log(error.message));
+const CONNECTION_URL = "mongodb://ak2711474:ak2711474@ac-pjsrkgs-shard-00-00.dxx9jxd.mongodb.net:27017,ac-pjsrkgs-shard-00-01.dxx9jxd.mongodb.net:27017,ac-pjsrkgs-shard-00-02.dxx9jxd.mongodb.net:27017/?ssl=true&replicaSet=atlas-6w9zwt-shard-0&authSource=admin&retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
 
-// mongoose.set('useFindAndModify', false);
-
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error));
